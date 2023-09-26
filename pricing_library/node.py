819a1f0a9d9eabfbbb0e5c5526a590613e8343ca
BaptisteZloch <bzloch@hotmail.fr>
2023-09-26 16:54:23 +0200

    in progress

--- pricing_library/node.py
+++ pricing_library/node.py
@@ -1,9 +1,8 @@
 from __future__ import annotations
-from typing import Literal, Optional
+from typing import Optional
 from math import exp
 import numpy as np
 from pricing_library.utils import discount_value, calculate_forward_price
-from pricing_library.trinomial_tree import TrinomialTree
 
 
 class Node:
@@ -13,100 +12,88 @@ class Node:
     def __init__(
         self,
         spot_price: float,
-        tree: TrinomialTree,
-        # interest_rates: float,
-        # delta_t: float,
-        # alpha: float,
-        # volatility: float,
-        # option_strike_price: float,
-        # option_type: Literal["call", "put"],
+        tree,
     ) -> None:
         self.tree = tree
-
         self.spot_price = spot_price
-
         self.forward_price = calculate_forward_price(
-            self.spot_price, interest_rates, delta_t
+            self.spot_price, tree.market.interest_rate, self.tree.delta_t
         )
-        self.up_price = alpha * self.forward_price
-        self.down_price = self.forward_price / alpha
+        self.up_price = self.tree.alpha * self.forward_price
+        self.down_price = self.forward_price / self.tree.alpha
 
-        n = Node(self.forward_price)
+        self.variance = self.calculate_variance()
+
+        self.p_down = self.calculate_p_down()
+        self.p_up = self.calculate_p_up()
+        self.p_mid = self.calculate_p_mid()
+
+        self.option_value = self.calculate_option_value()
+
+    def add_child_node(self):
+        n = Node(self.forward_price, self.tree)
         self.next_mid_node = n
-        n_up = Node(self.up_price)
+        n_up = Node(self.up_price, self.tree)
         self.next_upper_node = n_up
         n.node_up = n_up
 
-        n_down = Node(self.down_price)
+        n_down = Node(self.down_price, self.tree)
         self.next_lower_node = n_down
         n.node_down = n_up
 
-        self.variance = self.calculate_variance(volatility, delta_t, interest_rates)
-
-        self.p_down = self.calculate_p_down(alpha)
-        self.p_up = self.calculate_p_up(alpha)
-        self.p_mid = self.calculate_p_mid()
-
-        self.option_value = self.calculate_option_value(
-            option_strike_price, interest_rates, delta_t, option_type
-        )
-
     def calculate_option_value(
         self,
-        option_strike_price: float,
-        interest_rates: float,
-        delta_t: float,
-        option_type: Literal["put", "call"],
     ) -> float:
-        if option_type == "call":
+        if self.tree.option.option_type == "call":
             option_value = float(
                 np.array(
                     [
-                        max(option_strike_price - self.up_price, 0),
-                        max(option_strike_price - self.forward_price, 0),
-                        max(option_strike_price - self.down_price, 0),
+                        max(self.tree.option.strike_price - self.up_price, 0),
+                        max(self.tree.option.strike_price - self.forward_price, 0),
+                        max(self.tree.option.strike_price - self.down_price, 0),
                     ]
                 )
                 @ np.array([self.p_up, self.p_mid, self.p_down])
             )
-        elif option_type == "put":
+        elif self.tree.option.option_type == "put":
             option_value = float(
                 np.array(
                     [
-                        max(self.up_price - option_strike_price, 0),
-                        max(self.forward_price - option_strike_price, 0),
-                        max(self.down_price - option_strike_price, 0),
+                        max(self.up_price - self.tree.option.strike_price, 0),
+                        max(self.forward_price - self.tree.option.strike_price, 0),
+                        max(self.down_price - self.tree.option.strike_price, 0),
                     ]
                 )
                 @ np.array([self.p_up, self.p_mid, self.p_down])
             )
         else:
-            raise ValueError(
-                f"option_type must be either 'call' or 'put', not {option_type}."
-            )
-        return discount_value(option_value, interest_rates, delta_t)
-
-    def calculate_variance(
-        self, volatility: float, delta_t: float, interest_rates: float
-    ) -> float:
-        return (
-            (self.spot_price**2)
-            * exp(2 * interest_rates * delta_t)
-            * (exp(volatility**2 * delta_t) - 1)
+            raise ValueError(f"option_type must be either 'call' or 'put' only.")
+        return discount_value(
+            option_value, self.tree.market.interest_rate, self.tree.delta_t
         )
 
-    def calculate_p_up(self, alpha) -> float:
-        return self.p_down / alpha
+    def calculate_variance(self) -> float:
+        return (
+            (self.spot_price**2)
+            * exp(2 * self.tree.market.interest_rate * self.tree.delta_t)
+            * (exp(self.tree.market.volatility**2 * self.tree.delta_t) - 1)
+        )
+
+    def calculate_p_up(
+        self,
+    ) -> float:
+        return self.p_down / self.tree.alpha
 
     def calculate_p_mid(self) -> float:
         return 1 - (self.p_down + self.p_up)
 
-    def calculate_p_down(self, alpha) -> float:
+    def calculate_p_down(self) -> float:
         return (
             self.forward_price ** (-2) * (self.variance + self.forward_price**2)
             - 1
-            - (alpha + 1) * (self.forward_price ** (-1) * self.forward_price - 1)
-        ) / ((1 - alpha) * (alpha ** (-1) - 1))
+            - (self.tree.alpha + 1)
+            * (self.forward_price ** (-1) * self.forward_price - 1)
+        ) / ((1 - self.tree.alpha) * (self.tree.alpha ** (-1) - 1))
 
     def check_conditions(self):
         assert self.p_down + self.p_up + self.p_mid == 1, "Probabilities must sum to 1."
--- pricing_library/trinomial_tree.py
+++ pricing_library/trinomial_tree.py
@@ -13,7 +13,6 @@ class TrinomialTree:
         market: MarketData,
         option: Option,
         pricing_date: datetime,
-        maturity_date: datetime,
         n_steps: int,
     ) -> None:
         """_summary_
@@ -23,22 +22,22 @@ class TrinomialTree:
             market (MarketData): The MarketData object containing market informations.
             option (Option): The option associated with the trinomial tree.
             pricing_date (datetime): Pricing date of the option, could be today but must be before maturity_date.
-            maturity_date (datetime): The maturity date of the option.
             n_steps (int): The number of steps in the trinomial tree.
         """
         self.option = option
         self.market = market
-        self.root = Node(market.spot_price, self)  # S_0...
+
         # calculate delta t and alpha
         assert (
-            pricing_date < maturity_date
+            pricing_date < self.option.maturity_date
         ), "Pricing date must be before maturity date."
         self.n_steps = n_steps
         self.pricing_date = pricing_date
         self.delta_t = abs(
-            ((maturity_date - pricing_date).days / self.n_days) / n_steps
+            ((self.option.maturity_date - pricing_date).days / n_steps) / self.n_days
         )
         self.alpha = self.calculate_alpha()
+        self.root = Node(market.spot_price, self)  # S_0...
 
     def calculate_alpha(self) -> float:
         return exp(self.market.volatility * sqrt(3 * self.delta_t))
