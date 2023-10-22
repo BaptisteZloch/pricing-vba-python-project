8f6098b1dbe752867de2de73dd505a2c4ecd12c3
BaptisteZloch <bzloch@hotmail.fr>
2023-10-22 10:43:42 +0200

    bench in progress

--- run.py
+++ run.py
@@ -1,9 +1,16 @@
+from time import time
+from tqdm import tqdm
 from pricing_library.black_scholes import BlackScholesOptionPricing
 from pricing_library.market import Market
 from pricing_library.option import Option
 from pricing_library.trinomial_tree import TrinomialTree
 
 from datetime import datetime
+import sys 
+
+sys.setrecursionlimit(10**6) 
+
+
 opt=Option(
         option_type="call",
         exercise_type="eu",
@@ -18,15 +25,40 @@ mkt=Market(
         dividend_ex_date=datetime(2024, 5, 24),
     )
 pricing_date=datetime(2023, 9, 20)
+bs_price = BlackScholesOptionPricing(opt,pricing_date,mkt).calculate_option_price()
+with open(f"./result_gap_convergence_{mkt.spot_price}_{mkt.volatility}_{mkt.interest_rate}_{mkt.dividend_price}_{opt.option_type}_{opt.exercise_type}_{opt.strike_price}.csv",mode="w") as f:
+    f.write('steps,time,bs_price,price\n')
 
-t = TrinomialTree(
-    market=mkt,
-    pricing_date=pricing_date,
-    n_steps=100,
-)
 
-price = t.price(
-    opt=opt,
-    draw_tree=False
-)
-print(f"Trinomial price : {price} vs BS price : {BlackScholesOptionPricing(opt,pricing_date,mkt).calculate_option_price()}")
+for step in tqdm(range(1,100,10),desc="Computing convergence and GAP"):
+    t = TrinomialTree(
+        market=mkt,
+        pricing_date=pricing_date,
+        n_steps=step,
+    )
+    start_time = time()
+    price = t.price(
+        opt=opt,
+        draw_tree=False
+    )
+    end_time = time()
+    with open(f"./result_gap_convergence_{mkt.spot_price}_{mkt.volatility}_{mkt.interest_rate}_{mkt.dividend_price}_{opt.option_type}_{opt.exercise_type}_{opt.strike_price}.csv",mode="a") as f:
+        f.write(f"{step},{price},{bs_price},{end_time-start_time}\n")
+    del t
+
+
+for step in tqdm(range(100,2500,20),desc="Computing convergence and GAP"):
+    t = TrinomialTree(
+        market=mkt,
+        pricing_date=pricing_date,
+        n_steps=step,
+    )
+    start_time = time()
+    price = t.price(
+        opt=opt,
+        draw_tree=False
+    )
+    end_time = time()
+    with open(f"./result_gap_convergence_{mkt.spot_price}_{mkt.volatility}_{mkt.interest_rate}_{mkt.dividend_price}_{opt.option_type}_{opt.exercise_type}_{opt.strike_price}.csv",mode="a") as f:
+        f.write(f"{step},{price},{bs_price},{end_time-start_time}\n")
+    del t
