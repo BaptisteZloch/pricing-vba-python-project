1272bee117263b4871ea495f3a433d94dabdb38c
BaptisteZloch <bzloch@hotmail.fr>
2023-11-03 20:18:55 +0100

    gamma not ready yet

--- pricing_library/black_scholes.py
+++ pricing_library/black_scholes.py
@@ -4,9 +4,9 @@ from scipy.stats import norm
 from pricing_library.market import Market
 from pricing_library.option import Option
 
-class BlackScholesOptionPricing:
-    def __init__(self,opt:Option, pricing_date:datetime,market:Market):
 
+class BlackScholesOptionPricing:
+    def __init__(self, opt: Option, pricing_date: datetime, market: Market):
         self.S = market.spot_price
         self.K = opt.strike_price
         self.T = ((opt.maturity_date - pricing_date).days) / 365
@@ -15,7 +15,9 @@ class BlackScholesOptionPricing:
         self.option_type = opt.option_type
 
     def __d1(self):
-        d1 = (np.log(self.S / self.K) + (self.r + 0.5 * self.sigma**2) * self.T) / (self.sigma * np.sqrt(self.T))
+        d1 = (np.log(self.S / self.K) + (self.r + 0.5 * self.sigma**2) * self.T) / (
+            self.sigma * np.sqrt(self.T)
+        )
         return d1
 
     def __d2(self):
@@ -25,11 +27,36 @@ class BlackScholesOptionPricing:
     def calculate_option_price(self):
         d1 = self.__d1()
         d2 = self.__d2()
-        if self.option_type == 'call':
-            call_price = self.S * norm.cdf(d1) - self.K * np.exp(-self.r * self.T) * norm.cdf(d2)
+        if self.option_type == "call":
+            call_price = self.S * norm.cdf(d1) - self.K * np.exp(
+                -self.r * self.T
+            ) * norm.cdf(d2)
             return call_price
-        elif self.option_type == 'put':
-            put_price = self.K * np.exp(-self.r * self.T) * norm.cdf(-d2) - self.S * norm.cdf(-d1)
+        elif self.option_type == "put":
+            put_price = self.K * np.exp(-self.r * self.T) * norm.cdf(
+                -d2
+            ) - self.S * norm.cdf(-d1)
             return put_price
         else:
             raise ValueError("Option type not supported. Use 'call' or 'put'.")
+
+    def calculate_delta(self):
+        d1 = self.__d1()
+        if self.option_type == "call":
+            delta = norm.cdf(d1)
+            return delta
+        elif self.option_type == "put":
+            delta = norm.cdf(d1) - 1
+            return delta
+        else:
+            raise ValueError("Option type not supported. Use 'call' or 'put'.")
+
+    def calculate_gamma(self):
+        d1 = self.__d1()
+        gamma = norm.pdf(d1) / (self.S * self.sigma * np.sqrt(self.T))
+        return gamma
+
+    def calculate_vega(self):
+        d1 = self.__d1()
+        vega = self.S * norm.pdf(d1) * np.sqrt(self.T)
+        return vega
--- pricing_library/greeks.py
+++ pricing_library/greeks.py
@@ -10,18 +10,20 @@ def delta(
     pricing_date: datetime,
     opt: Option,
     h: float = 0.01,
-)->float:
-    """_summary_
+) -> float:
+    """Compute the delta of an option for a given market and a given pricing date.
 
     Args:
-        mkt (Market): _description_
-        n_steps (int): _description_
-        pricing_date (datetime): _description_
-        opt (Option): _description_
+    -----
+        mkt (Market): The market conditions used to price the option.
+        n_steps (int): The number of steps used to discretize the trinomial tree.
+        pricing_date (datetime): The date at which the option is priced.
+        opt (Option): The option to price and compute the delta.
         h (float, optional): The step used to discretize the volatility to compute the vega in percent i.e. : 0.01 => +/- 1%. Defaults to 0.01.
 
     Returns:
-        float: _description_
+    -----
+        float: The delta of an option for the given market and pricing date.
     """
     mkt_spot_up, mkt_spot_down = mkt.spot_price * (1 + h), mkt.spot_price * (1 - h)
 
@@ -51,16 +53,66 @@ def delta(
     return (price_up - price_down) / (mkt_spot_up - mkt_spot_down)
 
 
-# def gamma(opt: Option, mkt: Market, h: float = 0.01):
-#     """_summary_
+def gamma(
+    mkt: Market,
+    n_steps: int,
+    pricing_date: datetime,
+    opt: Option,
+    h: float = 0.01,
+) -> float:
+    """Compute the gamma of an option for a given market and a given pricing date.
 
-#     Args:
-#         opt (Option): _description_
-#         mkt (Market): _description_
-#         h (float): The step used to compute the delta in percent i.e. : 0.01=> +/- 1%. Defaults to 0.01.
-#     """
-#     mkt_spot_up, mkt_spot_down = mkt.spot_price * (1 + h), mkt.spot_price * (1 - h)
-#     pass
+    Args:
+    -----
+        mkt (Market): The market conditions used to price the option.
+        n_steps (int): The number of steps used to discretize the trinomial tree.
+        pricing_date (datetime): The date at which the option is priced.
+        opt (Option): The option to price and compute the delta.
+        h (float, optional): The step used to discretize the volatility to compute the vega in percent i.e. : 0.01 => +/- 1%. Defaults to 0.01.
+
+    Returns:
+    -----
+        float: The gamma of an option for the given market and pricing date.
+    """
+    mkt_spot_up, mkt_spot_down = mkt.spot_price * (1 + h), mkt.spot_price * (1 - h)
+
+    price_up = TrinomialTree(
+        Market(
+            mkt.interest_rate,
+            mkt.volatility,
+            mkt_spot_up,
+            mkt.dividend_price,
+            mkt.dividend_ex_date,
+        ),
+        pricing_date,
+        n_steps,
+    ).price(opt)
+    price_mid = TrinomialTree(
+        Market(
+            mkt.interest_rate,
+            mkt.volatility,
+            mkt.spot_price,
+            mkt.dividend_price,
+            mkt.dividend_ex_date,
+        ),
+        pricing_date,
+        n_steps,
+    ).price(opt)
+    price_down = TrinomialTree(
+        Market(
+            mkt.interest_rate,
+            mkt.volatility,
+            mkt_spot_down,
+            mkt.dividend_price,
+            mkt.dividend_ex_date,
+        ),
+        pricing_date,
+        n_steps,
+    ).price(opt)
+    delta_up = (price_up - price_mid) / (mkt_spot_up - mkt.spot_price)
+    delta_down = (price_mid - price_down) / (mkt.spot_price - mkt_spot_down)
+
+    return (delta_up - delta_down) / (mkt_spot_up - mkt_spot_down) ** 2
 
 
 def vega(
@@ -70,16 +122,18 @@ def vega(
     opt: Option,
     h: float = 0.01,
 ):
-    """_summary_
+    """Compute the vega of an option for a given market and a given pricing date.
 
     Args:
-        mkt (Market): _description_
-        n_steps (int): _description_
-        pricing_date (datetime): _description_
-        opt (Option): _description_
+    -----
+        mkt (Market): The market conditions used to price the option.
+        n_steps (int): The number of steps used to discretize the trinomial tree.
+        pricing_date (datetime): The date at which the option is priced.
+        opt (Option): The option to price and compute the delta.
         h (float, optional): The step used to discretize the volatility to compute the vega in percent i.e. : 0.01 => +/- 1%. Defaults to 0.01.
 
     Returns:
+    -----
         float: _description_
     """
     mkt_vol_up, mkt_vol_down = mkt.volatility * (1 + h), mkt.volatility * (1 - h)
