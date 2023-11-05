35267d6007af8395e8c06a1ee63b7386221303d7
BaptisteZloch <bzloch@hotmail.fr>
2023-11-05 11:44:49 +0100

    Project sounds okay

--- pricing_library/greeks.py
+++ pricing_library/greeks.py
@@ -4,161 +4,163 @@ from pricing_library.option import Option
 from pricing_library.trinomial_tree import TrinomialTree
 
 
-def delta(
-    mkt: Market,
-    n_steps: int,
-    pricing_date: datetime,
-    opt: Option,
-    h: float = 0.01,
-) -> float:
-    """Compute the delta of an option for a given market and a given pricing date.
+class Greeks:
+    @staticmethod
+    def delta(
+        mkt: Market,
+        n_steps: int,
+        pricing_date: datetime,
+        opt: Option,
+        h: float = 0.01,
+    ) -> float:
+        """Compute the delta of an option for a given market and a given pricing date.
 
-    Args:
-    -----
-        mkt (Market): The market conditions used to price the option.
-        n_steps (int): The number of steps used to discretize the trinomial tree.
-        pricing_date (datetime): The date at which the option is priced.
-        opt (Option): The option to price and compute the delta.
-        h (float, optional): The step used to discretize the volatility to compute the vega in percent i.e. : 0.01 => +/- 1%. Defaults to 0.01.
+        Args:
+        -----
+            mkt (Market): The market conditions used to price the option.
+            n_steps (int): The number of steps used to discretize the trinomial tree.
+            pricing_date (datetime): The date at which the option is priced.
+            opt (Option): The option to price and compute the delta.
+            h (float, optional): The step used to discretize the volatility to compute the vega in percent i.e. : 0.01 => +/- 1%. Defaults to 0.01.
 
-    Returns:
-    -----
-        float: The delta of an option for the given market and pricing date.
-    """
-    mkt_spot_up, mkt_spot_down = mkt.spot_price * (1 + h), mkt.spot_price * (1 - h)
+        Returns:
+        -----
+            float: The delta of an option for the given market and pricing date.
+        """
+        mkt_spot_up, mkt_spot_down = mkt.spot_price * (1 + h), mkt.spot_price * (1 - h)
 
-    price_up = TrinomialTree(
-        Market(
-            mkt.interest_rate,
-            mkt.volatility,
-            mkt_spot_up,
-            mkt.dividend_price,
-            mkt.dividend_ex_date,
-        ),
-        pricing_date,
-        n_steps,
-    ).price(opt)
-    price_down = TrinomialTree(
-        Market(
-            mkt.interest_rate,
-            mkt.volatility,
-            mkt_spot_down,
-            mkt.dividend_price,
-            mkt.dividend_ex_date,
-        ),
-        pricing_date,
-        n_steps,
-    ).price(opt)
+        price_up = TrinomialTree(
+            Market(
+                mkt.interest_rate,
+                mkt.volatility,
+                mkt_spot_up,
+                mkt.dividend_price,
+                mkt.dividend_ex_date,
+            ),
+            pricing_date,
+            n_steps,
+        ).price(opt)
+        price_down = TrinomialTree(
+            Market(
+                mkt.interest_rate,
+                mkt.volatility,
+                mkt_spot_down,
+                mkt.dividend_price,
+                mkt.dividend_ex_date,
+            ),
+            pricing_date,
+            n_steps,
+        ).price(opt)
 
-    return (price_up - price_down) / (mkt_spot_up - mkt_spot_down)
+        return (price_up - price_down) / (mkt_spot_up - mkt_spot_down)
 
+    @staticmethod
+    def gamma(
+        mkt: Market,
+        n_steps: int,
+        pricing_date: datetime,
+        opt: Option,
+        h: float = 0.01,
+    ) -> float:
+        """Compute the gamma of an option for a given market and a given pricing date.
 
-def gamma(
-    mkt: Market,
-    n_steps: int,
-    pricing_date: datetime,
-    opt: Option,
-    h: float = 0.01,
-) -> float:
-    """Compute the gamma of an option for a given market and a given pricing date.
+        Args:
+        -----
+            mkt (Market): The market conditions used to price the option.
+            n_steps (int): The number of steps used to discretize the trinomial tree.
+            pricing_date (datetime): The date at which the option is priced.
+            opt (Option): The option to price and compute the delta.
+            h (float, optional): The step used to discretize the volatility to compute the vega in percent i.e. : 0.01 => +/- 1%. Defaults to 0.01.
 
-    Args:
-    -----
-        mkt (Market): The market conditions used to price the option.
-        n_steps (int): The number of steps used to discretize the trinomial tree.
-        pricing_date (datetime): The date at which the option is priced.
-        opt (Option): The option to price and compute the delta.
-        h (float, optional): The step used to discretize the volatility to compute the vega in percent i.e. : 0.01 => +/- 1%. Defaults to 0.01.
+        Returns:
+        -----
+            float: The gamma of an option for the given market and pricing date.
+        """
+        mkt_spot_up, mkt_spot_down = mkt.spot_price * (1 + h), mkt.spot_price * (1 - h)
 
-    Returns:
-    -----
-        float: The gamma of an option for the given market and pricing date.
-    """
-    mkt_spot_up, mkt_spot_down = mkt.spot_price * (1 + h), mkt.spot_price * (1 - h)
+        price_up = TrinomialTree(
+            Market(
+                mkt.interest_rate,
+                mkt.volatility,
+                mkt_spot_up,
+                mkt.dividend_price,
+                mkt.dividend_ex_date,
+            ),
+            pricing_date,
+            n_steps,
+        ).price(opt)
+        price_mid = TrinomialTree(
+            Market(
+                mkt.interest_rate,
+                mkt.volatility,
+                mkt.spot_price,
+                mkt.dividend_price,
+                mkt.dividend_ex_date,
+            ),
+            pricing_date,
+            n_steps,
+        ).price(opt)
+        price_down = TrinomialTree(
+            Market(
+                mkt.interest_rate,
+                mkt.volatility,
+                mkt_spot_down,
+                mkt.dividend_price,
+                mkt.dividend_ex_date,
+            ),
+            pricing_date,
+            n_steps,
+        ).price(opt)
+        delta_up = (price_up - price_mid) / (mkt_spot_up - mkt.spot_price)
+        delta_down = (price_mid - price_down) / (mkt.spot_price - mkt_spot_down)
 
-    price_up = TrinomialTree(
-        Market(
-            mkt.interest_rate,
-            mkt.volatility,
-            mkt_spot_up,
-            mkt.dividend_price,
-            mkt.dividend_ex_date,
-        ),
-        pricing_date,
-        n_steps,
-    ).price(opt)
-    price_mid = TrinomialTree(
-        Market(
-            mkt.interest_rate,
-            mkt.volatility,
-            mkt.spot_price,
-            mkt.dividend_price,
-            mkt.dividend_ex_date,
-        ),
-        pricing_date,
-        n_steps,
-    ).price(opt)
-    price_down = TrinomialTree(
-        Market(
-            mkt.interest_rate,
-            mkt.volatility,
-            mkt_spot_down,
-            mkt.dividend_price,
-            mkt.dividend_ex_date,
-        ),
-        pricing_date,
-        n_steps,
-    ).price(opt)
-    delta_up = (price_up - price_mid) / (mkt_spot_up - mkt.spot_price)
-    delta_down = (price_mid - price_down) / (mkt.spot_price - mkt_spot_down)
+        return (delta_up - delta_down) / (mkt_spot_up - mkt_spot_down) ** 2
 
-    return (delta_up - delta_down) / (mkt_spot_up - mkt_spot_down) ** 2
+    @staticmethod
+    def vega(
+        mkt: Market,
+        n_steps: int,
+        pricing_date: datetime,
+        opt: Option,
+        h: float = 0.01,
+    ):
+        """Compute the vega of an option for a given market and a given pricing date.
 
+        Args:
+        -----
+            mkt (Market): The market conditions used to price the option.
+            n_steps (int): The number of steps used to discretize the trinomial tree.
+            pricing_date (datetime): The date at which the option is priced.
+            opt (Option): The option to price and compute the delta.
+            h (float, optional): The step used to discretize the volatility to compute the vega in percent i.e. : 0.01 => +/- 1%. Defaults to 0.01.
 
-def vega(
-    mkt: Market,
-    n_steps: int,
-    pricing_date: datetime,
-    opt: Option,
-    h: float = 0.01,
-):
-    """Compute the vega of an option for a given market and a given pricing date.
+        Returns:
+        -----
+            float: _description_
+        """
+        mkt_vol_up, mkt_vol_down = mkt.volatility * (1 + h), mkt.volatility * (1 - h)
 
-    Args:
-    -----
-        mkt (Market): The market conditions used to price the option.
-        n_steps (int): The number of steps used to discretize the trinomial tree.
-        pricing_date (datetime): The date at which the option is priced.
-        opt (Option): The option to price and compute the delta.
-        h (float, optional): The step used to discretize the volatility to compute the vega in percent i.e. : 0.01 => +/- 1%. Defaults to 0.01.
+        price_up = TrinomialTree(
+            Market(
+                mkt.interest_rate,
+                mkt_vol_up,
+                mkt.spot_price,
+                mkt.dividend_price,
+                mkt.dividend_ex_date,
+            ),
+            pricing_date,
+            n_steps,
+        ).price(opt)
+        price_down = TrinomialTree(
+            Market(
+                mkt.interest_rate,
+                mkt_vol_down,
+                mkt.spot_price,
+                mkt.dividend_price,
+                mkt.dividend_ex_date,
+            ),
+            pricing_date,
+            n_steps,
+        ).price(opt)
 
-    Returns:
-    -----
-        float: _description_
-    """
-    mkt_vol_up, mkt_vol_down = mkt.volatility * (1 + h), mkt.volatility * (1 - h)
-
-    price_up = TrinomialTree(
-        Market(
-            mkt.interest_rate,
-            mkt_vol_up,
-            mkt.spot_price,
-            mkt.dividend_price,
-            mkt.dividend_ex_date,
-        ),
-        pricing_date,
-        n_steps,
-    ).price(opt)
-    price_down = TrinomialTree(
-        Market(
-            mkt.interest_rate,
-            mkt_vol_down,
-            mkt.spot_price,
-            mkt.dividend_price,
-            mkt.dividend_ex_date,
-        ),
-        pricing_date,
-        n_steps,
-    ).price(opt)
-
-    return (price_up - price_down) / (mkt_vol_up - mkt_vol_down)
+        return (price_up - price_down) / (mkt_vol_up - mkt_vol_down)
--- run.py
+++ run.py
@@ -6,59 +6,62 @@ from pricing_library.option import Option
 from pricing_library.trinomial_tree import TrinomialTree
 
 from datetime import datetime
-import sys 
+import sys
 
-sys.setrecursionlimit(10**6) 
-
-
-opt=Option(
-        option_type="call",
-        exercise_type="eu",
-        strike_price=102,
-        maturity_date=datetime(2024, 9, 19),
-    )
-mkt=Market(
-        interest_rate=0.04,
-        volatility=0.25,
-        spot_price=100,
-        dividend_price=0,
-        dividend_ex_date=datetime(2024, 5, 24),
-    )
-pricing_date=datetime(2023, 9, 20)
-bs_price = BlackScholesOptionPricing(opt,pricing_date,mkt).calculate_option_price()
-with open(f"./result_gap_convergence_{mkt.spot_price}_{mkt.volatility}_{mkt.interest_rate}_{mkt.dividend_price}_{opt.option_type}_{opt.exercise_type}_{opt.strike_price}.csv",mode="w") as f:
-    f.write('steps,time,bs_price,price\n')
+sys.setrecursionlimit(10**6)
 
 
-for step in tqdm(range(1,100,10),desc="Computing convergence and GAP"):
+opt = Option(
+    option_type="call",
+    exercise_type="eu",
+    strike_price=102,
+    maturity_date=datetime(2024, 9, 19),
+)
+mkt = Market(
+    interest_rate=0.04,
+    volatility=0.25,
+    spot_price=100,
+    dividend_price=0,
+    dividend_ex_date=datetime(2024, 5, 24),
+)
+pricing_date = datetime(2023, 9, 20)
+bs_price = BlackScholesOptionPricing(opt, pricing_date, mkt).calculate_option_price()
+with open(
+    f"./benchmarks/result_gap_convergence_{mkt.spot_price}_{mkt.volatility}_{mkt.interest_rate}_{mkt.dividend_price}_{opt.option_type}_{opt.exercise_type}_{opt.strike_price}.csv",
+    mode="w",
+) as f:
+    f.write("steps,time,bs_price,price\n")
+
+
+for step in tqdm(range(1, 100, 10), desc="Computing convergence and GAP"):
     t = TrinomialTree(
         market=mkt,
         pricing_date=pricing_date,
         n_steps=step,
     )
     start_time = time()
-    price = t.price(
-        opt=opt,
-        draw_tree=False
-    )
+    price = t.price(opt=opt, draw_tree=False)
     end_time = time()
-    with open(f"./result_gap_convergence_{mkt.spot_price}_{mkt.volatility}_{mkt.interest_rate}_{mkt.dividend_price}_{opt.option_type}_{opt.exercise_type}_{opt.strike_price}.csv",mode="a") as f:
+    with open(
+        f"./benchmarks/result_gap_convergence_{mkt.spot_price}_{mkt.volatility}_{mkt.interest_rate}_{mkt.dividend_price}_{opt.option_type}_{opt.exercise_type}_{opt.strike_price}.csv",
+        mode="a",
+    ) as f:
         f.write(f"{step},{price},{bs_price},{end_time-start_time}\n")
     del t
 
 
-for step in tqdm(range(100,2500,20),desc="Computing convergence and GAP"):
+for step in tqdm(range(100, 2500, 20), desc="Computing convergence and GAP"):
     t = TrinomialTree(
         market=mkt,
         pricing_date=pricing_date,
         n_steps=step,
     )
     start_time = time()
-    price = t.price(
-        opt=opt,
-        draw_tree=False
-    )
+    price = t.price(opt=opt, draw_tree=False)
     end_time = time()
-    with open(f"./result_gap_convergence_{mkt.spot_price}_{mkt.volatility}_{mkt.interest_rate}_{mkt.dividend_price}_{opt.option_type}_{opt.exercise_type}_{opt.strike_price}.csv",mode="a") as f:
+    with open(
+        f"./benchmarks/result_gap_convergence_{mkt.spot_price}_{mkt.volatility}_{mkt.interest_rate}_{mkt.dividend_price}_{opt.option_type}_{opt.exercise_type}_{opt.strike_price}.csv",
+        mode="a",
+    ) as f:
         f.write(f"{step},{price},{bs_price},{end_time-start_time}\n")
     del t
